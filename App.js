fc675fd419eef3aa5c2e174b1b830c7f358eeb9d
Enrico Donelli <enrico.donelli@gmail.com>
2017-06-09 09:02:46 +0200

    ripartenza

--- App.js
+++ App.js
@@ -1,74 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
-
-class Hello extends Component {
-  render() {
-    return (
-      <div> ciao exio</div>
-    )
-  }
-};
-
 
 export default class App extends React.Component {
   render() {
     return (
-      <Container>
-        <Header>
-                    <Left>
-                         <Button transparent>
-                            <Icon name='menu' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Text>body</Text>
-                    </Body>
-                    <Right >
-                        <Text>enrico</Text>
-                      </Right>
-                </Header>
-        <Content>
-        <Text>apri up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        </Content>
-        <Footer>
-                    <FooterTab>
-                          <Button>
-                              <Icon name="apps" />
-                          </Button>
-                          <Button>
-                              <Icon name="camera" />
-                          </Button>
-                          <Button active>
-                              <Icon active name="navigate" />
-                          </Button>
-                          <Button>
-                              <Icon name="person" />
-                          </Button>
-                      </FooterTab>
-                </Footer>
-      </Container>
+      </View>
     );
-  }
-}
-
-class Wrapper extends React.Component {
-  state = { fontsAreLoaded: false };
-
-  async componentWillMount() {
-    await Exponent.Font.loadAsync({
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
-    });
-    this.setState({fontsAreLoaded: true});
-  }
-
-  render() {
-    if (this.state.fontsAreLoaded)
-      return <App/>;
-    else
-      return <Exponent.Components.AppLoading/>;
   }
 }
 
