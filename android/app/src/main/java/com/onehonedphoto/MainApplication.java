1eed9e582dae58a10ca0c1351440a183903e8179
Ezio Vergine <ezio.vergine@gmail.com>
2017-09-08 16:10:16 +0200

    fix icons ionicons support on ios

--- android/app/src/main/java/com/onehonedphoto/MainApplication.java
+++ android/app/src/main/java/com/onehonedphoto/MainApplication.java
@@ -3,6 +3,7 @@ package com.onehonedphoto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
             new RCTCameraPackage()
         );
